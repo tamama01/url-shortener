523409646ea6c62cd194343995e3a1b8d927b51b
Your Name <you@example.com>
2026-01-18 14:46:44 +0800

    Add gunicorn and update port

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def redirect_url(short_code):
     return "Link not found", 404
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
